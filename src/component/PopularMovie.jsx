f5e3bd8e868f0963ccdd2e28fde9eb7813142394
Your Name <kenneth1.kodehode@gmail.com>
2023-01-02 09:37:47 +0100

    update

--- src/component/PopularMovie.jsx
+++ src/component/PopularMovie.jsx
@@ -8,9 +8,9 @@ function PopularMovie (props) {
         <>
         <div className="w-96 inline-flex flex-col ">
 
-            {/* Why does it return an extra div (undefined), after the array ?? */}
+            {/* Why does it return an extra div (GET: Undefined), after the array ?? */}
             <img className="my-4 m-auto" src={(`https://www.themoviedb.org/t/p/w220_and_h330_face/${props.poster_path}`)} />
-     
+
             <h4 className="text-3xl mb-2">{props.title}</h4>
             <p className=" w-80 m-auto">{props.overview}</p>
         </div>
